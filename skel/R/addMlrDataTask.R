a3369d5824d42677bfd7ec48fea2142adcb7b184
berndbischl <bernd_bischl@gmx.net>
2012-07-26 19:05:55 +0000

    bemlr: mlr changes

--- skel/R/addMlrDataTask.R
+++ skel/R/addMlrDataTask.R
@@ -12,14 +12,22 @@
 #' @param measures [\code{\link[mlr]{Measure}} | list of \code{\link[mlr]{Measure}}]\cr
 #'   Performance measures to evaluate for task.
 #'   Default are the default \pkg{mlr} measures for the task.
+#' @param naomit [\code{logical(1)}]\cr
+#'   Should \code{\link{na.omit}} be called on the data to remove rows with missings?
+#'   Default is \code{FALSE}.
 #' @param seed [\code{integer(1)}]\cr
 #'   Problem seed.
 #'   Default is to generate a random one.
 #' @return [\code{character(1)}]. Invisibly returns the id.
 #' @export
-addMlrDataTask = function(reg, id, resampling, measures, seed) {
+addMlrDataTask = function(reg, id, resampling, measures, naomit=FALSE, seed) {
   require(mlrData)
   checkArg(id, "character", len=1, na.ok=FALSE)
-  env = getDataset(id=id, task="train", assign=FALSE)
+  # FIXME change arg in getDataset
+  if (naomit)
+    nafun = na.omit
+  else
+    nafun = na.pass
+  env = getDataset(id=id, task="train", assign=FALSE, na.action=nafun)
   addTask(reg, env$task, resampling, measures, seed)
 }
