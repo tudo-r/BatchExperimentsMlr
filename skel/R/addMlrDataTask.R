63952786eaf07e15eb8e516c3d44e262cebbee74
berndbischl <bernd_bischl@gmx.net>
2012-07-27 16:17:36 +0000

    bemlr

--- skel/R/addMlrDataTask.R
+++ skel/R/addMlrDataTask.R
@@ -12,22 +12,16 @@
 #' @param measures [\code{\link[mlr]{Measure}} | list of \code{\link[mlr]{Measure}}]\cr
 #'   Performance measures to evaluate for task.
 #'   Default are the default \pkg{mlr} measures for the task.
-#' @param naomit [\code{logical(1)}]\cr
-#'   Should \code{\link{na.omit}} be called on the data to remove rows with missings?
-#'   Default is \code{FALSE}.
 #' @param seed [\code{integer(1)}]\cr
 #'   Problem seed.
 #'   Default is to generate a random one.
+#' @param ... [any]\cr
+#'   Further arguments passed to \code{\link[mlrData]{getDataset}}.
 #' @return [\code{character(1)}]. Invisibly returns the id.
 #' @export
-addMlrDataTask = function(reg, id, resampling, measures, naomit=FALSE, seed) {
+addMlrDataTask = function(reg, id, resampling, measures, seed, ...) {
   require(mlrData)
   checkArg(id, "character", len=1, na.ok=FALSE)
-  # FIXME change arg in getDataset
-  if (naomit)
-    nafun = na.omit
-  else
-    nafun = na.pass
-  env = getDataset(id=id, task="train", assign=FALSE, na.action=nafun)
+  env = getDataset(id=id, task="train", assign=FALSE, ...)
   addTask(reg, env$task, resampling, measures, seed)
 }
--- skel/R/addMlrTask.R
+++ skel/R/addMlrTask.R
@@ -12,20 +12,11 @@
 #' @param measures [\code{\link[mlr]{Measure}} | list of \code{\link[mlr]{Measure}}]\cr
 #'   Performance measures to evaluate for task.
 #'   Default are the default \pkg{mlr} measures for the task.
-#' @param naomit [\code{logical(1)}]\cr
-#'   Should \code{\link{na.omit}} be called on the data to remove rows with missings?
-#'   Default is \code{FALSE}.
 #' @param seed [\code{integer(1)}]\cr
 #'   Problem seed.
 #'   Default is to generate a random one.
 #' @return [\code{character(1)}]. Invisibly returns the id.
 #' @export
-addMlrTask = function(reg, task, resampling, measures, naomit=FALSE, seed) {
-  # FIXME: test this
-  if (naomit) {
-    data = getTaskData(task)
-    data = na.omit(data)
-    mlr:::changeData(task, data)
-  }
+addMlrTask = function(reg, task, resampling, measures, seed) {
   addTask(reg, task, resampling, measures, seed)
 }
