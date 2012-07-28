10cf0a99231b296b242551a5a920570422d621f9
berndbischl <bernd_bischl@gmx.net>
2012-07-28 23:54:18 +0000

    bemlr

--- skel/R/reduceResultsMlr.R
+++ skel/R/reduceResultsMlr.R
@@ -2,11 +2,20 @@
 #' @param reg [\code{\link{ExperimentRegistryMlr}}]\cr
 #'   Experiment registry.
 #' @param ids [\code{integer}]\cr
-#'   Ids of selected experiments. Default is all jobs for which results are available.
+#'   Ids of selected experiments. 
+#'   Default is all jobs for which results are available.
+#' @param strings.as.factors [\code{logical(1)}]
+#'   Should all character columns in result be converted to factors?
+#'   See \code{\link{reduceResultsExperiments}}.
+#'   Default is \code{default.stringsAsFactors()}.
+#' @param block.size [\code{integer(1)}]
+#'   Results will be fetched in blocks of size \code{block.size}.
+#'   See \code{\link{reduceResultsExperiments}}.
+#'   Default is 100.
 #' @return [\code{data.frame}]. Contains everything implied by \code{\link{reduceResultsExperiments}} and
 #'   all measures defined in added problems.
 #' @export
-reduceResultsMlr = function(reg, ids) {
+reduceResultsMlr = function(reg, ids, strings.as.factors=default.stringsAsFactors(), block.size=100) {
   checkArg(reg, "ExperimentRegistryMlr")
   # FIXME: maybe include train measures
   res = reduceResultsExperiments(reg, ids, 
